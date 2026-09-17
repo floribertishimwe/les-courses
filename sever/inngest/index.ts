3698a95d9da2a6a241f0438d6ec05f77e6a70208
Floribert <ifloribert2000@gmail.com>
2026-09-17 15:42:26 +0200

    added node mailer

--- sever/inngest/index.ts
+++ sever/inngest/index.ts
@@ -1,7 +1,32 @@
 import { Inngest } from "inngest";
+import { prisma } from "../config/prisma.js";
+
+const LOW_STOCK_TRESHOLD = 10;
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "glocery-delivery"});
 
-// Create an empty array where we'll export future Inngest functions
+// Low stock alert to Admin Email
+const checkLowerStock = inngest.createFunction(
+  { id: "check-low-stock",
+    name:"Low Stock Alert" ,
+    triggers: [{event: "inventory/stock.updated"}] },
+  async ({ event, step }) => {
+
+    const { productId } = event.data;  
+    const product = await step.run('fetch-product', async ()=>{
+        return await prisma.product.findUnique({
+            where: {id: productId}
+        }) 
+    } )
+
+    if (!product || product.stock === null || product.stock >= LOW_STOCK_TRESHOLD ){
+      return { skipped: true, stock: product?.stock }
+    } 
+
+    await step.run( "send-low-stock-email", async()=>{
+        const adminEmails = process.env.ADMIN_EMAILS ? process.env.ADMIN_EMAILS.split(",").map((e)=>e.trim()) : []
+        if(adminEmails.length === 0 )  return { skipped: true, reason:"No admin emails"}
+    });
+},);
 export const functions = [];
